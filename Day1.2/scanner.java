f2151c1071e000e0f11ca9176a0ce58a54b11661
shivmundhe7 <shivmundhe16@gmail.com>
2025-09-09 11:08:05 +0530

    DSA

---
+++ Day1.2/scanner.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class scanner {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter 2 numbers for multiplication");
+        double num1 = sc.nextDouble();
+        double num2 = sc.nextDouble();
+        System.out.println(num1 * num2);
+    }
+    }
