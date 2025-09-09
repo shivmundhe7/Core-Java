83baf2da41d55ddaf200a051cf38e1f61ae67a1b
shivmundhe7 <shivmundhe16@gmail.com>
2025-09-10 00:08:57 +0530

    Day 1.2

---
+++ Day1.2/Datatypes/Primitivedatatypes.java
@@ -0,0 +1,9 @@
+public class PrimitiveDatatypes {
+    public static void main(String[] args) {
+        int count = 100;
+        if(count==100);
+            System.out.println("YESSS");
+        else{
+            System.out.println("NO");}
+    }
+}
